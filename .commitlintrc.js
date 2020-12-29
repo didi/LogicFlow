6b7b94f7f3d8a97efc4d8199b642114c692910f3
Chengran.Y <yanchengran@didiglobal.com>
2020-12-29 13:05:02 +0800

    chore: 修改commitlint配置

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -12,10 +12,8 @@ module.exports = {
           'never',
           ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
       ],
-      'subject-empty': [2, 'never'],
       'subject-full-stop': [2, 'never', '.'],
       'type-case': [2, 'always', 'lower-case'],
-      'type-empty': [2, 'never'],
       'type-enum': [
           2,
           'always',
