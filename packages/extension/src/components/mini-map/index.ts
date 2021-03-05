9994903d23507ea26806a7f4141181f7484706c5
xutao <taoxuxutao@didiglobal.com>
2021-03-05 13:57:35 +0800

    feat: add mini map extension

---
+++ packages/extension/src/components/mini-map/index.ts
@@ -0,0 +1,28 @@
+const MiniMap = {
+  __lf: null,
+  __container: null,
+  install() {},
+  render(lf, container) {
+    console.log(lf);
+    MiniMap.__lf = lf;
+    MiniMap.__container = container;
+  },
+  __createMiniMap(left = 100, top = 100) {
+    console.log(1);
+    const miniMapWrap = document.createElement('div');
+    miniMapWrap.className = 'lf-mini-map';
+    miniMapWrap.style.left = `${left}px`;
+    miniMapWrap.style.top = `${top}px`;
+    MiniMap.__container.appendChild(miniMapWrap);
+  },
+  show(left, top) {
+    this.__createMiniMap(left, top);
+    console.log(2);
+  }
+};
+
+export default MiniMap;
+
+export {
+  MiniMap,
+};
