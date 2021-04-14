a2969927c68088bbe26792c30a568fecb7e89237
liuchao <yhlchao@163.com>
2021-04-14 17:01:12 +0800

    chore: change the code style of webpack config in core
    affects: @logicflow/core

--- packages/core/scripts/webpack.config.base.js
+++ packages/core/scripts/webpack.config.base.js
@@ -1,53 +1,53 @@
-const path = require('path')
+const path = require('path');
 // const VueLoaderPlugin = require('vue-loader/lib/plugin')
-const { version } = require('../package.json')
 
 module.exports = {
-  entry: path.resolve(__dirname, "../src/index.tsx"),
+  entry: path.resolve(__dirname, '../src/index.tsx'),
   output: {
-    path: path.resolve(__dirname, "../"),
-    filename: `logic-flow.js`,
+    path: path.resolve(__dirname, '../'),
+    filename: 'logic-flow.js',
     libraryTarget: 'umd',
   },
   resolve: {
     alias: {
-      "react": "preact/compat",
-      "react-dom/test-utils": "preact/test-utils",
-      "react-dom": "preact/compat",
+      react: 'preact/compat',
+      'react-dom/test-utils': 'preact/test-utils',
+      'react-dom': 'preact/compat',
     },
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".less", ".css"]
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.less', '.css'],
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx|ts|tsx)$/,
         exclude: /node_modules/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-typescript',
-                {
-                  isTSX: true,
-                  allExtensions: true,
-                  jsxPragma: "h"
-                }
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-typescript',
+                  {
+                    isTSX: true,
+                    allExtensions: true,
+                    jsxPragma: 'h',
+                  },
+                ],
+                [
+                  '@babel/preset-env',
+                  {
+                    useBuiltIns: 'usage',
+                    corejs: '3.3',
+                  },
+                ],
               ],
-              [
-                '@babel/preset-env',
-                {
-                  "useBuiltIns": "usage",
-                  "corejs": '3.3'
-                }
-              ]
-            ],
-          }
-        },
-        // {
-        //   loader: 'eslint-loader'
-        // }
-      ]
+            },
+          },
+          // {
+          //   loader: 'eslint-loader'
+          // }
+        ],
       },
       {
         test: /\.css$/,
@@ -57,16 +57,16 @@ module.exports = {
             loader: 'css-loader',
             options: {
               modules: true,
-            }
+            },
           },
-        ]
+        ],
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 100000000
-        }
+          limit: 100000000,
+        },
       },
       {
         test: /\.less$/,
@@ -76,10 +76,10 @@ module.exports = {
             loader: 'css-loader',
           },
           {
-            loader: 'less-loader'
+            loader: 'less-loader',
           },
-        ]
-      }
-    ]
-  }
-}
+        ],
+      },
+    ],
+  },
+};
--- packages/core/scripts/webpack.config.dev.js
+++ packages/core/scripts/webpack.config.dev.js
@@ -1,68 +1,39 @@
-const path = require('path')
-const webpack = require('webpack')
-const baseWebpackConfig = require('./webpack.config.base.js')
-const packagesConfig = require('../../extension/scripts/webpack.config.base.js')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
-const getProxy = require('./config')
+const baseWebpackConfig = require('./webpack.config.base.js');
+const getProxy = require('./config');
 // const packagesEntry = require('./packages.config');
 
 // 先不用webpack merge
 module.exports = [
-  Object.assign({}, baseWebpackConfig, {
+  {
+    ...baseWebpackConfig,
     devtool: 'inline-source-map',
     mode: 'development',
     devServer: {
-      contentBase: path.join(__dirname, "../"),
+      contentBase: path.join(__dirname, '../'),
       stats: 'errors-warnings',
       port: 9090,
       host: 'localhost',
       watchOptions: {
-        poll: true
+        poll: true,
       },
-      proxy: getProxy()
+      proxy: getProxy(),
     },
     plugins: [
       new HtmlWebpackPlugin({
         filename: 'index.html',
-        template: path.resolve(__dirname, "../index.html")
+        template: path.resolve(__dirname, '../index.html'),
       }),
       new CaseSensitivePathsPlugin(),
       new ESLintPlugin({
-        context: path.resolve(__dirname, "../src"),
-        extensions: ['ts', 'tsx']
+        context: path.resolve(__dirname, '../src'),
+        extensions: ['ts', 'tsx'],
       }),
-      new webpack.EnvironmentPlugin(['NODE_ENV', 'MOCK_TYPE'])
-    ]
-  }),
-  Object.assign({}, packagesConfig, {
-    devtool: 'inline-source-map',
-    mode: 'development',
-    output: {
-      path: path.resolve(__dirname, "../lib"),
-      filename: `[name].js`,
-      libraryTarget: 'umd',
-      libraryExport: 'default', // 兼容 ES6(ES2015) 的模块系统、CommonJS 和 AMD 模块规范
-    },
-    plugins: [
-      new CaseSensitivePathsPlugin(),
-      new webpack.EnvironmentPlugin(['NODE_ENV', 'MOCK_TYPE'])
-    ]
-  })
-  // Object.assign({}, baseWebpackConfig, {
-  //   devtool: 'inline-source-map',
-  //   mode: 'development',
-  //   entry: packagesEntry,
-  //   output: {
-  //     path: path.resolve(__dirname, "../lib"),
-  //     filename: `[name].js`,
-  //     libraryTarget: 'umd',
-  //     libraryExport: 'default', // 兼容 ES6(ES2015) 的模块系统、CommonJS 和 AMD 模块规范
-  //   },
-  //   plugins: [
-  //     new CaseSensitivePathsPlugin(),
-  //     new webpack.EnvironmentPlugin(['NODE_ENV', 'MOCK_TYPE'])
-  //   ]
-  // })
-]
+      new webpack.EnvironmentPlugin(['NODE_ENV', 'MOCK_TYPE']),
+    ],
+  },
+];
