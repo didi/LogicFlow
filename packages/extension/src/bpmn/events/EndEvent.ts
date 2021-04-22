df25d110eee7051ee9357b11f669d80a3de1e0ea
liuchao <yhlchao@163.com>
2021-04-22 20:23:19 +0800

    fix: update registerElement API in extension
    affects: @logicflow/core, @logicflow/extension

--- packages/extension/src/bpmn/events/EndEvent.ts
+++ packages/extension/src/bpmn/events/EndEvent.ts
@@ -1,72 +1,64 @@
+import { CircleNode, CircleNodeModel, h } from '@logicflow/core';
 import { getBpmnId } from '../getBpmnId';
-// import { CircleNodeModel } from 'logic-flow';
-import {
-  CircleNodeModelContractor,
-  CircleNodeViewContractor,
-  // EventEventModel,
-} from '../../type/index';
 
-export const getEndEventModel = (
-  CircleNodeModel: CircleNodeModelContractor,
-): CircleNodeModelContractor => {
-  class EndEventModel extends CircleNodeModel {
-    static extendKey = 'EndEventModel';
-    constructor(data, graphModel) {
-      if (!data.id) {
-        data.id = `Event_${getBpmnId()}`;
-      }
-      if (!data.text) {
-        data.text = {
-          value: '',
-          x: data.x,
-          y: data.y + 40,
-        };
-      }
-      if (data.text && typeof data.text === 'string') {
-        data.text = {
-          value: data.text,
-          x: data.x,
-          y: data.y + 40,
-        };
-      }
-      super(data, graphModel);
+class EndEventModel extends CircleNodeModel {
+  static extendKey = 'EndEventModel';
+  constructor(data, graphModel) {
+    if (!data.id) {
+      data.id = `Event_${getBpmnId()}`;
     }
-    getConnectedSourceRules() {
-      const rules = super.getConnectedSourceRules();
-      const notAsSource = {
-        message: '结束节点不能作为连线的起点',
-        validate: () => false,
+    if (!data.text) {
+      data.text = {
+        value: '',
+        x: data.x,
+        y: data.y,
       };
-      rules.push(notAsSource);
-      return rules;
     }
+    if (data.text && typeof data.text === 'string') {
+      data.text = {
+        value: data.text,
+        x: data.x,
+        y: data.y,
+      };
+    }
+    data.text.y += 40;
+    super(data, graphModel);
   }
-  return EndEventModel;
+  getConnectedSourceRules() {
+    const rules = super.getConnectedSourceRules();
+    const notAsSource = {
+      message: '结束节点不能作为连线的起点',
+      validate: () => false,
+    };
+    rules.push(notAsSource);
+    return rules;
+  }
+}
+
+class EndEventView extends CircleNode {
+  getShape() {
+    const { x, y, fill, stroke, strokeWidth, r } = this.getAttributes();
+    const outCircle = super.getShape();
+    return h(
+      'g',
+      {},
+      outCircle,
+      h('circle', {
+        cx: x,
+        cy: y,
+        fill,
+        stroke,
+        strokeWidth,
+        r: r - 5,
+      }),
+    );
+  }
+}
+
+const EndEvent = {
+  view: EndEventView,
+  model: EndEventModel,
 };
 
-export const getEndEventView = (
-  CircleNode: CircleNodeViewContractor, h,
-): CircleNodeViewContractor => {
-  class EndEventNode extends CircleNode {
-    getShape() {
-      const {
-        x, y, fill, stroke, strokeWidth, r,
-      } = this.getAttributes();
-      const outCircle = super.getShape();
-      return h(
-        'g',
-        {},
-        outCircle,
-        h('circle', {
-          cx: x,
-          cy: y,
-          fill,
-          stroke,
-          strokeWidth,
-          r: r - 5,
-        }),
-      );
-    }
-  }
-  return EndEventNode;
-};
+export { EndEventView, EndEventModel };
+export default EndEvent;
--- packages/extension/src/bpmn/gateways/ExclusiveGateway.ts
+++ packages/extension/src/bpmn/gateways/ExclusiveGateway.ts
@@ -1,79 +1,80 @@
+import { h, PolygonNode, PolygonNodeModel } from '@logicflow/core';
 import { getBpmnId } from '../getBpmnId';
-import {
-  PolygonNodeModelContractor,
-  PolygonNodeViewContractor,
-  ExclusiveGatewayAttribute,
-} from '../../type/index';
+import { ExclusiveGatewayAttribute } from '../../type/index';
 
-export const getExclusiveGateway = (
-  PolygonNodeModel: PolygonNodeModelContractor,
-): PolygonNodeModelContractor => {
-  class ExclusiveGatewayModel extends PolygonNodeModel {
-    static extendKey = 'ExclusiveGatewayModel';
-    constructor(data, graphModel) {
-      if (!data.id) {
-        data.id = `Gateway_${getBpmnId()}`;
-      }
-      if (!data.text) {
-        data.text = {
-          value: '',
-          x: data.x,
-          y: data.y + 40,
-        };
-      }
-      if (data.text && typeof data.text === 'string') {
-        data.text = {
-          value: data.text,
-          x: data.x,
-          y: data.y + 40,
-        };
-      }
-      super(data, graphModel);
-      this.points = [
-        [25, 0],
-        [50, 25],
-        [25, 50],
-        [0, 25],
-      ];
+class ExclusiveGatewayModel extends PolygonNodeModel {
+  static extendKey = 'ExclusiveGatewayModel';
+  constructor(data, graphModel) {
+    if (!data.id) {
+      data.id = `Gateway_${getBpmnId()}`;
     }
+    if (!data.text) {
+      data.text = {
+        value: '',
+        x: data.x,
+        y: data.y,
+      };
+    }
+    if (data.text && typeof data.text === 'string') {
+      data.text = {
+        value: data.text,
+        x: data.x,
+        y: data.y,
+      };
+    }
+    data.text.y += 40;
+    super(data, graphModel);
+    this.points = [
+      [25, 0],
+      [50, 25],
+      [25, 50],
+      [0, 25],
+    ];
   }
-  return ExclusiveGatewayModel;
+}
+
+class ExclusiveGatewayView extends PolygonNode {
+  static extendKey = 'ExclusiveGatewayNode';
+  getShape() {
+    const attributes = super.getAttributes() as ExclusiveGatewayAttribute;
+    const {
+      x,
+      y,
+      fill,
+      stroke,
+      strokeWidth,
+      width,
+      height,
+      points,
+    } = attributes;
+    return h(
+      'g',
+      {
+        transform: `matrix(1 0 0 1 ${x - width / 2} ${y - height / 2})`,
+      },
+      h('polygon', {
+        fill,
+        x,
+        y,
+        stroke,
+        strokeWidth,
+        points,
+      }),
+      h('path', {
+        fill,
+        stroke,
+        strokeWidth,
+        d:
+          'm 16,15 7.42857142857143,9.714285714285715 -7.42857142857143,9.714285714285715 3.428571428571429,0 5.714285714285715,-7.464228571428572 5.714285714285715,7.464228571428572 3.428571428571429,0 -7.42857142857143,-9.714285714285715 7.42857142857143,-9.714285714285715 -3.428571428571429,0 -5.714285714285715,7.464228571428572 -5.714285714285715,-7.464228571428572 -3.428571428571429,0 z',
+      }),
+    );
+  }
+}
+
+const ExclusiveGateway = {
+  view: ExclusiveGatewayView,
+  model: ExclusiveGatewayModel,
 };
 
-export const getExclusiveGatewayView = (
-  PolygonNode: PolygonNodeViewContractor, h,
-): PolygonNodeViewContractor => {
-  class ExclusiveGatewayNode extends PolygonNode {
-    static extendKey = 'ExclusiveGatewayNode';
-    getShape() {
-      const attributes = super.getAttributes() as ExclusiveGatewayAttribute;
-      const {
-        x, y, fill, stroke, strokeWidth, width, height, points,
-      } = attributes;
-      return h(
-        'g',
-        {
-          transform: `matrix(1 0 0 1 ${x - width / 2} ${y - height / 2})`,
-        },
-        h(
-          'polygon',
-          {
-            fill,
-            x,
-            y,
-            stroke,
-            strokeWidth,
-            points,
-          },
-        ),
-        h('path', {
-          fill,
-          stroke,
-          strokeWidth,
-          d: 'm 16,15 7.42857142857143,9.714285714285715 -7.42857142857143,9.714285714285715 3.428571428571429,0 5.714285714285715,-7.464228571428572 5.714285714285715,7.464228571428572 3.428571428571429,0 -7.42857142857143,-9.714285714285715 7.42857142857143,-9.714285714285715 -3.428571428571429,0 -5.714285714285715,7.464228571428572 -5.714285714285715,-7.464228571428572 -3.428571428571429,0 z',
-        }),
-      );
-    }
-  }
-  return ExclusiveGatewayNode;
-};
+export { ExclusiveGatewayView, ExclusiveGatewayModel };
+export default ExclusiveGateway;
