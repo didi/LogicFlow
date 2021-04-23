37f769d5566de067ab8cc369352c37443e195b08
liuchao <yhlchao@163.com>
2021-04-23 11:48:42 +0800

    refactor: change the register to registerElement in packages/site
    affects: site

---
+++ packages/site/src/element/rect.ts
@@ -0,0 +1,22 @@
+import { RectNode, RectNodeModel } from '@logicflow/core';
+
+class CustomRectModel extends RectNodeModel {
+  getConnectedSourceRules() {
+    const rules = super.getConnectedSourceRules();
+    const rule = {
+      message: '不满足连线的校验规则',
+      validate: (source, target) => {
+        // 校验规则
+        console.log(target);
+        return true;
+      },
+    };
+    rules.push(rule);
+    return rules;
+  }
+}
+
+export const Rect = {
+  view: RectNode,
+  model: CustomRectModel,
+};
