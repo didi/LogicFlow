f0ff960a2518cfe0e3f9d2a5abb843b87957def6
towersxu <taoxuxutao@didiglobal.com>
2021-11-22 16:18:31 +0800

    feat: export context menu

--- packages/extension/src/index.ts
+++ packages/extension/src/index.ts
@@ -5,6 +5,7 @@ export * from './turbo-adapter';
 export * from './insert-node-in-polyline';
 export * from './components/control';
 export * from './components/menu';
+export * from './components/context-menu';
 export * from './components/dnd-panel';
 export * from './components/selection-select';
 export * from './components/mini-map';
