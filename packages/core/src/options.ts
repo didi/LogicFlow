141c6678503870a9d7503864353ad6cc7493fd24
xutao <towersxu@gmail.com>
2022-02-10 10:01:39 +0800

    feat: check width and height type

--- packages/core/src/options.ts
+++ packages/core/src/options.ts
@@ -108,9 +108,12 @@ export interface GuardsTypes {
 
 // 用来获取用户传入的 options，并做一些容错和异常抛出
 export function get(options: Definition) {
-  const { container, grid } = options;
+  const { container, grid, width, height } = options;
   if (!container) {
     throw new Error('请检查 container 参数是否有效');
+  }
+  if (typeof width === 'string' || typeof height === 'string') {
+    throw new Error('width或height不支持传入字符串，请传数字');
   }
   if (grid) {
     options.grid = assign({
