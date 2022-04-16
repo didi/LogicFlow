e170533ff2273aac3d493781594c21d72bc97535
xutao <towersxu@gmail.com>
2022-04-16 11:06:15 +0800

    fix #555: move nodes having better alignment performance

--- packages/core/src/util/geometry.ts
+++ packages/core/src/util/geometry.ts
@@ -1,8 +1,8 @@
-export function snapToGrid(distance: number, gridSize: number) {
+export function snapToGrid(point: number, gridSize: number) {
   // 保证 x, y 的值为 gridSize 的整数倍
-  return gridSize * Math.round(distance / gridSize) || distance;
+  return gridSize * Math.round(point / gridSize) || point;
 }
-// 获取节点偏移是，产生的偏移量。当节点基于gridSize进行了偏移后，
+// 获取节点偏移时，产生的偏移量。当节点基于gridSize进行了偏移后，
 // 节点上的文本可以基于此方法移动对应的距离来保持与节点相对位置不变。
 export function getGridOffset(distance: number, gridSize: number) {
   return distance % gridSize;
