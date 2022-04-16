88a7dd8fb357d658299de52328cf878d43a300ce
Zhang Xingjian <1312144019@qq.com>
2022-04-16 15:58:50 +0800

    feat: english locale extension

---
+++ packages/extension/src/locale/en-locale/en.ts
@@ -0,0 +1,22 @@
+const en = {
+  文本: 'text',
+  开始: 'start',
+  缩小流程图: 'zoom in',
+  缩小: 'zoom in',
+  放大流程图: 'zoom out',
+  放大: 'zoom out',
+  恢复流程原有尺寸: 'adapt to original size',
+  适应: 'adapt',
+  回到上一步: 'undo',
+  上一步: 'undo',
+  移到下一步: 'redo',
+  下一步: 'redo',
+
+  删除: 'delete',
+  编辑文本: 'edit text',
+  复制: 'copy',
+};
+
+export default en;
+
+export { en };
