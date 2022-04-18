cacf346dcc225bcbfcdb3601a61cd11ab1084d4e
xutao <towersxu@gmail.com>
2022-04-18 10:49:31 +0800

    fix #572: add faker node model virtual property

--- packages/core/src/LogicFlow.tsx
+++ packages/core/src/LogicFlow.tsx
@@ -988,6 +988,9 @@ export default class LogicFlow {
     }
     // * initNodeData区分是否为虚拟节点
     const fakerNodeModel = new Model({ ...nodeConfig, virtual: true }, this.graphModel);
+    // fix #572: 由于正常节点不关心此属性，不会将其加入model。
+    // 但是上面的constructor还是要传，历史原因，有人会在constructor中基于virtual属性进行额外处理。
+    fakerNodeModel.virtual = true;
     this.graphModel.setFakerNode(fakerNodeModel);
     return fakerNodeModel;
   }
