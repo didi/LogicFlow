fa6ad3ff2c36a9a7979e03eb89ba2ca8c66b8a0b
xutao <towersxu@gmail.com>
2022-05-07 15:37:00 +0800

    fix: remove unnecessary object

--- packages/core/src/view/basic-shape/Path.tsx
+++ packages/core/src/view/basic-shape/Path.tsx
@@ -8,16 +8,11 @@ type IProps = {
 function Path(props: IProps) {
   const attrs = {
     d: '',
-    ...props,
   };
   Object.entries(props).forEach(([k, v]) => {
-    if (k === 'style') {
+    const valueType = typeof v;
+    if (k === 'style' || valueType !== 'object') {
       attrs[k] = v;
-    } else {
-      const valueType = typeof v;
-      if (valueType !== 'object') {
-        attrs[k] = v;
-      }
     }
   });
   return (
