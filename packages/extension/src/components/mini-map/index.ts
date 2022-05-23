c7fc211438715528651ef4923849a793c325a2c8
xutao <towersxu@gmail.com>
2022-05-23 11:23:38 +0800

    fix: use blank:drop instead of graph:transform

--- packages/extension/src/components/mini-map/index.ts
+++ packages/extension/src/components/mini-map/index.ts
@@ -35,7 +35,7 @@ class MiniMap {
         this.__setView();
       }
     });
-    this.__lf.on('graph:transform', () => {
+    this.__lf.on('blank:drop', () => {
       if (this.__isShow) {
         this.__setView();
       }
