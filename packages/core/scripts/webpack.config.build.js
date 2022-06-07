1c654152787647894045b6d4a8bfbefa676e470a
coderEDC <coderEDC@163.com>
2022-06-08 00:22:14 +0800

    chore: provide uncompression build file for development environment
    affects: @logicflow/core
    
    ISSUES CLOSED: #561

--- packages/core/scripts/webpack.config.build.js
+++ packages/core/scripts/webpack.config.build.js
@@ -2,23 +2,41 @@ const path = require('path');
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const baseWebpackConfig = require('./webpack.config.base.js');
-
+const TerserJSPlugin = require('terser-webpack-plugin');
 const { analyse } = process.env;
 const plugins = [new CaseSensitivePathsPlugin()];
 if (analyse) {
   plugins.push(new BundleAnalyzerPlugin());
 }
-
+const entryPath = path.resolve(__dirname, '../src/index.tsx');
 module.exports = [
   {
     ...baseWebpackConfig,
     mode: 'production',
-    entry: path.resolve(__dirname, '../src/index.tsx'),
+    entry: {
+      'logic-flow': entryPath,
+      'logic-flow.min': entryPath,
+    },
     output: {
       path: path.resolve(__dirname, '../dist'),
-      filename: 'logic-flow.js',
+      filename: '[name].js',
       libraryTarget: 'umd',
     },
     plugins,
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserJSPlugin({
+          include: /\.min\.js$/,
+          extractComments: false,
+          terserOptions: {
+            compress: {
+              drop_console: true,
+              pure_funcs: ['console.log'],
+            },
+          },
+        }),
+      ],
+    },
   },
 ];
