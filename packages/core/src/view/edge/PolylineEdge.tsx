3a017c8855d66cb444388267de6c090bc0fb0c89
xutao <987427795@qq.com>
2022-09-29 14:59:51 +0800

    feat: keep typo

--- packages/core/src/view/edge/PolylineEdge.tsx
+++ packages/core/src/view/edge/PolylineEdge.tsx
@@ -77,7 +77,7 @@ export default class PolylineEdge extends BaseEdge {
     this.appendInfo = appendInfo;
   };
   // 是否正在拖拽，在折线调整时，不展示起终点的调整点
-  getIsDragging = () => this.isDragging;
+  getIsDragging = () => this.isDraging;
   getEdge() {
     const { model } = this.props;
     const { points, isAnimation, arrowConfig } = model;
