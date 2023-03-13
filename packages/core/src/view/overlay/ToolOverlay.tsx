9be1f58a3768fd48117e1143939c8a92f61191dc
xutao <987427795@qq.com>
2023-03-13 15:59:32 +0800

    fix: trigger extension render in difference env

--- packages/core/src/view/overlay/ToolOverlay.tsx
+++ packages/core/src/view/overlay/ToolOverlay.tsx
@@ -10,12 +10,15 @@ type IProps = {
 
 @observer
 export default class ToolOverlay extends Component<IProps> {
+  // 在react严格模式下，useEffect会执行两次，但是在LogicFlow内部，则只会触发一次componentDidMount和componentDidUpdate。
+  // 其中第一次componentDidMount对应的graphModel为被丢弃的graphModel, 所以不应该生效。
+  // 在非react环境下，只会触发一次componentDidMount，不会触发componentDidUpdate。
+  // 所以这里采用componentDidUpdate和componentDidMount都去触发插件的render方法。
+  componentDidMount(): void {
+    this.triggerToolRender();
+  }
   componentDidUpdate(): void {
-    const { tool, graphModel } = this.props;
-    const ToolOverlayElement = document.querySelector(`#ToolOverlay_${graphModel.flowId}`) as HTMLElement;
-    const lf: LogicFlow = tool.getInstance();
-    lf.components.forEach(render => render(lf, ToolOverlayElement));
-    lf.components = []; // 保证extension组件的render只执行一次
+    this.triggerToolRender();
   }
   /**
    * 外部传入的一般是HTMLElement
@@ -29,6 +32,13 @@ export default class ToolOverlay extends Component<IProps> {
     }));
     tool.components = components;
     return components;
+  }
+  triggerToolRender() {
+    const { tool, graphModel } = this.props;
+    const ToolOverlayElement = document.querySelector(`#ToolOverlay_${graphModel.flowId}`) as HTMLElement;
+    const lf: LogicFlow = tool.getInstance();
+    lf.components.forEach(render => render(lf, ToolOverlayElement));
+    lf.components = []; // 保证extension组件的render只执行一次
   }
   render() {
     const { graphModel } = this.props;
