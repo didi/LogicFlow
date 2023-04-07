502abde9155c692f95e0de9c8e9171806de29701
xutao <987427795@qq.com>
2023-04-07 18:28:08 +0800

    feat: use deepclone instead of json parse(#304)

--- packages/core/examples/customNode/index.mjs
+++ packages/core/examples/customNode/index.mjs
@@ -14,6 +14,9 @@ const lf = new LogicFlow({
   width: 1000,
   grid: false,
   height: 600,
+  keyboard: {
+    enabled: true
+  },
   background: {
     // filter: 'grayscale(100%)'
     backgroundImage: 'url(https://cdn.jsdelivr.net/gh/towersxu/cdn@latest/knowledge/animal/horse.png)',
@@ -39,78 +42,183 @@ lf.register(customHtml);
 lf.register(userTask);
 
 lf.render({
-  nodes: [
+  "nodes": [
     {
-      id: 'custom-111',
-      type: 'UserTask',
-      x: 700,
-      y: 100,
-      properties: {
-        disabled: true
-      }
+      "id": "93242a5c-c2fd-4837-8f74-bbe08d77c702",
+      "type": "customRect",
+      "x": 100,
+      "y": 100,
+      "properties": {},
+      "width": 100,
+      "height": 80
     },
     {
-      id: 'custom-112',
-      type: 'customCircle',
-      x: 500,
-      y: 100
+      "id": "f6ca2ae5-2a28-4881-8d06-4381c1e6d039",
+      "type": "customRect",
+      "x": 345,
+      "y": 100,
+      "properties": {},
+      "width": 100,
+      "height": 80
     },
     {
-      id: 'custom-114',
-      type: 'diamondNode',
-      x: 300,
-      y: 100
+      "id": "0a7d4f92-e0e9-4855-8dd9-2b78b839be72",
+      "type": "customRect",
+      "x": 323,
+      "y": 250,
+      "properties": {},
+      "width": 100,
+      "height": 80
     },
     {
-      id: 'custom-115',
-      type: 'ellipseNode',
-      x: 100,
-      y: 100
+      "id": "b7c4323b-89fe-4b59-9778-30964b6e5a4b",
+      "type": "customRect",
+      "x": 610,
+      "y": 100,
+      "properties": {},
+      "width": 100,
+      "height": 80
     },
     {
-      id: 'custom-116',
-      type: 'polygonNode',
-      x: 900,
-      y: 100
-    },
-    {
-      id: 'custom-61',
-      type: 'hexagonNode',
-      x: 200,
-      y: 300,
-    },
-    {
-      id: 'custom-62',
-      type: 'hexagonNode',
-      x: 500,
-      y: 500,
-    },
-    {
-      id: 'custom-117',
-      type: 'textNode',
-      x: 600,
-      y: 300,
-      text: '叽叽叽叽',
-      properties: {
-        isSelected: true
-      }
-    },
-    {
-      id: 'custom-118',
-      type: 'customHtml',
-      x: 800,
-      y: 300,
+      "id": "793fdbcc-c1b1-4427-9fb1-fad9329d6085",
+      "type": "customRect",
+      "x": 611,
+      "y": 251,
+      "properties": {},
+      "width": 100,
+      "height": 80
     }
   ],
-  edges: [
+  "edges": [
     {
-      type: 'polyline',
-      id: 'edge-1',
-      sourceNodeId: 'custom-61',
-      targetNodeId: 'custom-62',
+      "id": "db24c3d7-66cc-4403-af61-693cadcc7576",
+      "type": "polyline",
+      "sourceNodeId": "93242a5c-c2fd-4837-8f74-bbe08d77c702",
+      "targetNodeId": "f6ca2ae5-2a28-4881-8d06-4381c1e6d039",
+      "startPoint": {
+        "x": 150,
+        "y": 100
+      },
+      "endPoint": {
+        "x": 295,
+        "y": 100
+      },
+      "properties": {},
+      "pointsList": [
+        {
+          "x": 150,
+          "y": 100
+        },
+        {
+          "x": 295,
+          "y": 100
+        }
+      ]
+    },
+    {
+      "id": "4e1a2827-9afa-4731-b21b-4468975e09f7",
+      "type": "polyline",
+      "sourceNodeId": "93242a5c-c2fd-4837-8f74-bbe08d77c702",
+      "targetNodeId": "0a7d4f92-e0e9-4855-8dd9-2b78b839be72",
+      "startPoint": {
+        "x": 150,
+        "y": 100
+      },
+      "endPoint": {
+        "x": 273,
+        "y": 250
+      },
+      "properties": {},
+      "pointsList": [
+        {
+          "x": 150,
+          "y": 100,
+        },
+        {
+          "x": 273,
+          "y": 250
+        }
+      ]
+    },
+    {
+      "id": "e73fc5e7-c1a2-4e7f-8cde-6307cd60c5fb",
+      "type": "polyline",
+      "sourceNodeId": "f6ca2ae5-2a28-4881-8d06-4381c1e6d039",
+      "targetNodeId": "0a7d4f92-e0e9-4855-8dd9-2b78b839be72",
+      "startPoint": {
+        "x": 395,
+        "y": 100
+      },
+      "endPoint": {
+        "x": 273,
+        "y": 250
+      },
+      "properties": {},
+      "pointsList": [
+        {
+          "x": 395,
+          "y": 100
+        },
+        {
+          "x": 273,
+          "y": 250
+        }
+      ]
+    },
+    {
+      "id": "9301f104-6ad3-4d4b-83b8-73b2038627bc",
+      "type": "polyline",
+      "sourceNodeId": "0a7d4f92-e0e9-4855-8dd9-2b78b839be72",
+      "targetNodeId": "b7c4323b-89fe-4b59-9778-30964b6e5a4b",
+      "startPoint": {
+        "x": 373,
+        "y": 330
+      },
+      "endPoint": {
+        "x": 610,
+        "y": 100
+      },
+      "properties": {},
+      "pointsList": [
+        {
+          "x": 373,
+          "y": 330
+        },
+        {
+          "x": 610,
+          "y": 100
+        }
+      ]
+    },
+    {
+      "id": "2372042b-70f9-4c4a-b4c4-2f30d3254494",
+      "type": "polyline",
+      "sourceNodeId": "b7c4323b-89fe-4b59-9778-30964b6e5a4b",
+      "targetNodeId": "793fdbcc-c1b1-4427-9fb1-fad9329d6085",
+      "startPoint": {
+        "x": 710,
+        "y": 100
+      },
+      "endPoint": {
+        "x": 711,
+        "y": 251
+      },
+      "properties": {},
+      "pointsList": [
+        {
+          "x": 710,
+          "y": 100
+        },
+        {
+          "x": 711,
+          "y": 251
+        }
+      ]
     }
   ]
-});
+}
+
+);
 
 lf.on('anchor:dragstart', ({ data, nodeModel }) => {
   if (nodeModel.type === 'hexagonNode') {
@@ -147,7 +255,7 @@ document.querySelector('#select_js').addEventListener('click', () => {
 })
 
 document.querySelector('#get_data').addEventListener('click', () => {
-  console.log(lf.getGraphData())
+  console.log(JSON.stringify(lf.getGraphData()))
 })
 
 document.body.addEventListener('mousedown', () => {
--- packages/core/src/util/compatible.ts
+++ packages/core/src/util/compatible.ts
@@ -1,9 +1,11 @@
+import { cloneDeep } from 'lodash-es';
+
 // 需要进行兼容操作，提供的方法
 
 // vue中data会进行Observe, 深拷贝的原始数据对象。
 export const formatData = (data) => {
   try {
-    return JSON.parse(JSON.stringify(data));
+    return cloneDeep(data);
   } catch {
     return data;
   }
