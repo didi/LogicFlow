684a5812eddc0c108a2f5c88ea971889f9371e22
xutao <987427795@qq.com>
2023-05-13 12:11:16 +0800

    fix: keep use JSON.parse format input data.(#1153)

--- packages/core/src/util/compatible.ts
+++ packages/core/src/util/compatible.ts
@@ -1,11 +1,10 @@
-import { cloneDeep } from 'lodash-es';
-
-// 需要进行兼容操作，提供的方法
-
+// import { cloneDeep } from 'lodash-es';
 // vue中data会进行Observe, 深拷贝的原始数据对象。
 export const formatData = (data) => {
   try {
-    return cloneDeep(data);
+    // WARNING: cloneDeep虽然也会将Observe对象转换为plain对象，但是不会像JSON.parse那样，会将undefined去掉。
+    // 会导致后面的pick因为存在pick覆盖默认值的情况。
+    return JSON.parse(JSON.stringify(data));
   } catch {
     return data;
   }
