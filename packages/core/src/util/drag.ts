9961082775d6d5fa9d459e1ac7a611a8fc0bfb6b
boyongjiong <boyongjiong@didiglobal.com>
2023-11-21 17:07:10 +0800

    fix(core): fix Cannot read property 'document' of undefined bug(#1433)

--- packages/core/src/util/drag.ts
+++ packages/core/src/util/drag.ts
@@ -6,66 +6,8 @@ import { IBaseModel } from '../model';
 // import BaseNodeModel from '../model/node/BaseNodeModel';
 // import { snapToGrid } from './geometry';
 
-const DOC = window.document;
+// const DOC = window.document;
 const LEFT_MOUSE_BUTTON_CODE = 0;
-
-function createDrag({
-  onDragStart = noop,
-  onDragging = noop,
-  onDragEnd = noop,
-  step = 1,
-  isStopPropagation = true,
-}) {
-  let isDragging = false;
-  let isStartDragging = false;
-  let startX = 0;
-  let startY = 0;
-  let sumDeltaX = 0;
-  let sumDeltaY = 0;
-  function handleMouseMove(e: MouseEvent) {
-    if (isStopPropagation) e.stopPropagation();
-    if (!isStartDragging) return;
-    isDragging = true;
-    sumDeltaX += e.clientX - startX;
-    sumDeltaY += e.clientY - startY;
-    startX = e.clientX;
-    startY = e.clientY;
-    if (Math.abs(sumDeltaX) > step || Math.abs(sumDeltaY) > step) {
-      const remainderX = sumDeltaX % step;
-      const remainderY = sumDeltaY % step;
-      const deltaX = sumDeltaX - remainderX;
-      const deltaY = sumDeltaY - remainderY;
-      sumDeltaX = remainderX;
-      sumDeltaY = remainderY;
-      onDragging({ deltaX, deltaY, event: e });
-    }
-  }
-
-  function handleMouseUp(e: MouseEvent) {
-    if (isStopPropagation) e.stopPropagation();
-    isStartDragging = false;
-    DOC.removeEventListener('mousemove', handleMouseMove, false);
-    DOC.removeEventListener('mouseup', handleMouseUp, false);
-    if (!isDragging) return;
-    isDragging = false;
-    return onDragEnd({ event: e });
-  }
-
-  function handleMouseDown(e: MouseEvent) {
-    if (e.button !== LEFT_MOUSE_BUTTON_CODE) return;
-    if (isStopPropagation) e.stopPropagation();
-
-    isStartDragging = true;
-    startX = e.clientX;
-    startY = e.clientY;
-
-    DOC.addEventListener('mousemove', handleMouseMove, false);
-    DOC.addEventListener('mouseup', handleMouseUp, false);
-    return onDragStart({ event: e });
-  }
-
-  return handleMouseDown;
-}
 
 // 支持拖拽的时候，按照指定step进行。
 // 因为在绘制的过程中因为放大缩小，移动的真实的step则是变化的。
@@ -114,6 +56,7 @@ class StepDrag {
     this.model = model;
   }
   handleMouseDown = (e: MouseEvent) => {
+    const DOC: any = window?.document;
     if (e.button !== LEFT_MOUSE_BUTTON_CODE) return;
     if (this.isStopPropagation) e.stopPropagation();
     this.isStartDragging = true;
@@ -162,6 +105,8 @@ class StepDrag {
     }
   };
   handleMouseUp = (e: MouseEvent) => {
+    const DOC = window.document;
+
     this.isStartDragging = false;
     if (this.isStopPropagation) e.stopPropagation();
     // fix #568: 如果onDragging在下一个事件循环中触发，而drop在当前事件循环，会出现问题。
@@ -177,6 +122,8 @@ class StepDrag {
     });
   };
   cancelDrag = () => {
+    const DOC: any = window?.document;
+
     DOC.removeEventListener('mousemove', this.handleMouseMove, false);
     DOC.removeEventListener('mouseup', this.handleMouseUp, false);
     this.onDragEnd({ event: null });
@@ -185,6 +132,5 @@ class StepDrag {
 }
 
 export {
-  createDrag,
   StepDrag,
 };
