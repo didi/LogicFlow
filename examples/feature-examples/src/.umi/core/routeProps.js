865c31f8e0e874d791f29c632f29e34f30c201bf
boyongjiong <boyongjiong@didiglobal.com>
2024-01-04 16:14:56 +0800

    refactor: 重构 LogicFlow 项目结构，升级打包工具，使用 pnpm 做包管理工具

---
+++ examples/feature-examples/src/.umi/core/routeProps.js
@@ -0,0 +1,5 @@
+// src/.umi/core/routeProps.ts
+var routeProps_default = {};
+export {
+  routeProps_default as default
+};
--- site/mvp/vue.config.js
+++
@@ -1,8 +0,0 @@
-// vue.config.js
-
-/**
- * @type {import('@vue/cli-service').ProjectOptions}
- */
-module.exports = {
-  publicPath: './'
-}
