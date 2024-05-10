c7cd10154d3b5beed98325b2254b2840118b4a18
boyongjiong <boyongjiong@outlook.com>
2024-05-10 14:56:20 +0800

    refactor: core 包中类型定义完善，解决在 extension 包重构过程中遇到的 Core 包问题

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,6 @@
 // 统一对外导出
 import { observer as mobxObserver } from 'mobx-preact'
-import { createElement as h } from 'preact/compat'
+import { createElement as h, Component } from 'preact/compat'
 import LogicFlow from './LogicFlow'
 
 import * as LogicFlowUtil from './util'
@@ -9,7 +9,7 @@ export function observer<P>(props: P) {
   return mobxObserver(props as any)
 }
 
-export { LogicFlow, h, LogicFlowUtil }
+export { LogicFlow, h, Component, LogicFlowUtil }
 
 export * from './view'
 export * from './model'
