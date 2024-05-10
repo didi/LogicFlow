015908ba154c7bdfa379f0b642dbb4ab5f90c850
boyongjiong <boyongjiong@outlook.com>
2024-05-10 17:36:03 +0800

    docs: add feature-examples DEMO 项目，用于汇总所有官方 demo，后续再 docs 中使用

---
+++ examples/feature-examples/src/pages/nodes/custom/Html/data.ts
@@ -0,0 +1,17 @@
+const data = {
+  nodes: [
+    {
+      id: '1',
+      type: 'customHtml',
+      x: 300,
+      y: 150,
+      properties: {
+        name: 'hello',
+        body: 'world',
+      },
+    },
+  ],
+  edges: [],
+}
+
+export default data
