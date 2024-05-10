ee2b8b766e0aa959f375ebdaf7bc680500cfb58c
boyongjiong <boyongjiong@outlook.com>
2024-05-10 15:00:12 +0800

    docs: update docs

--- sites/docs/src/tutorial/getting-started/helloworld/index.tsx
+++ sites/docs/src/tutorial/getting-started/helloworld/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import LogicFlow from '@logicflow/core';
 import '@logicflow/core/es/index.css';
-// import '@logicflow/core/dist/style/index.css';
+// import '@logicflow/core/dist/style/index.less';
 
 import data from './data';
 import '../index.less';
