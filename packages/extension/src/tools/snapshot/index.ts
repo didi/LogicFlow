38c1193b6c180101e4962fcb669938780fb217c4
lylyn <17521027078@163.com>
2024-06-23 18:32:08 +0800

    feat(extension): 支持导出节点中的网络图片

--- packages/extension/src/tools/snapshot/index.ts
+++ packages/extension/src/tools/snapshot/index.ts
@@ -74,9 +74,10 @@ export class Snapshot {
   }
 
   /* 下载图片 */
-  getSnapshot(fileName: string, backgroundColor: string) {
+  async getSnapshot(fileName: string, backgroundColor: string) {
     this.fileName = fileName || `logic-flow.${Date.now()}.png`
     const svg = this.getSvgRootElement(this.lf)
+    await updateImageSource(svg)
     this.getCanvasData(svg, backgroundColor).then(
       (canvas: HTMLCanvasElement) => {
         const imgURI = canvas
@@ -88,8 +89,9 @@ export class Snapshot {
   }
 
   /* 获取base64对象 */
-  getSnapshotBase64(backgroundColor: string) {
+  async getSnapshotBase64(backgroundColor: string) {
     const svg = this.getSvgRootElement(this.lf)
+    await updateImageSource(svg)
     return new Promise((resolve) => {
       this.getCanvasData(svg, backgroundColor).then(
         (canvas: HTMLCanvasElement) => {
@@ -106,8 +108,9 @@ export class Snapshot {
   }
 
   /* 获取Blob对象 */
-  getSnapshotBlob(backgroundColor: string) {
+  async getSnapshotBlob(backgroundColor: string) {
     const svg = this.getSvgRootElement(this.lf)
+    await updateImageSource(svg)
     return new Promise((resolve) => {
       this.getCanvasData(svg, backgroundColor).then(
         (canvas: HTMLCanvasElement) => {
@@ -273,3 +276,133 @@ export class Snapshot {
 }
 
 export default Snapshot
+
+/**
+ * 图片缓存, 已请求过的图片直接从缓存中获取
+ */
+const imageCache: Record<string, string> = {}
+
+/**
+ * 当获取图片失败时会返回失败信息，是 text/plain 类型的数据
+ * @param str - 图片内容
+ * @returns
+ */
+function isTextPlainBase64(str: string) {
+  return str.startsWith('data:text/plain')
+}
+
+/**
+ * 将网络图片转为 base64
+ * @param url - 图片地址
+ * @returns
+ */
+async function convertImageToBase64(url: string): Promise<string> {
+  if (imageCache[url]) {
+    return imageCache[url]
+  }
+  try {
+    return fetch(url)
+      .then((response) => response.blob())
+      .then(
+        (blob) =>
+          new Promise((resolve, reject) => {
+            const reader = new FileReader()
+            reader.onloadend = () => {
+              resolve((imageCache[url] = reader.result as string))
+            }
+            reader.onerror = reject
+            reader.readAsDataURL(blob)
+          }),
+      )
+  } catch (error) {
+    // 如果转换失败，后续大概率仍然会失败，因此直接缓存
+    return (imageCache[url] = url)
+  }
+}
+
+/**
+ * 使用 base64 的图片替换 img 标签的 src 或 image 标签的 href
+ * @param node - html 节点或 svg 节点
+ */
+async function updateImageSrcOrHrefWithBase64Image(
+  node: HTMLImageElement | SVGImageElement,
+  attrName: 'src' | 'href',
+) {
+  try {
+    const url = node.getAttribute(attrName) || ''
+    // 已经是 base64 图片，不需要处理
+    if (url.startsWith('data:')) {
+      return
+    }
+    const base64Image = await convertImageToBase64(url)
+    if (isTextPlainBase64(base64Image)) {
+      return
+    }
+    node.setAttribute(attrName, base64Image)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+/**
+ * 使用 base64 的图片替换背景图片
+ * @param node - html 节点
+ * @param styleAttr - 样式属性名称
+ */
+async function updateBackgroundImageWithBase64Image(
+  node: HTMLElement,
+  url: string,
+) {
+  try {
+    // 已经是 base64 图片，不需要处理
+    if (url.startsWith('data:')) {
+      return
+    }
+    const base64Image = await convertImageToBase64(url)
+    if (isTextPlainBase64(base64Image)) {
+      return
+    }
+    node.style.backgroundImage = `url(${base64Image})`
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+/**
+ * 更新图片数据
+ * @param node - 节点
+ */
+async function updateImageSource(node: HTMLElement | SVGElement) {
+  const nodes = [node]
+  let nodePtr
+  const promises: any[] = []
+  while (nodes.length) {
+    nodePtr = nodes.shift()
+    if (nodePtr.children.length) {
+      nodes.push(...nodePtr.children)
+    }
+    if (nodePtr instanceof HTMLElement) {
+      // 如果有 style 的 background, backgroundImage 属性中有 url(xxx), 尝试替换为 base64 图片
+      const { background, backgroundImage } = nodePtr.style
+      const backgroundUrlMatch = background.match(/url\(["']?(.*?)["']?\)/)
+      if (backgroundUrlMatch && backgroundUrlMatch[1]) {
+        const imageUrl = backgroundUrlMatch[1]
+        promises.push(updateBackgroundImageWithBase64Image(nodePtr, imageUrl))
+      }
+      const backgroundImageUrlMatch = backgroundImage.match(
+        /url\(["']?(.*?)["']?\)/,
+      )
+      if (backgroundImageUrlMatch && backgroundImageUrlMatch[1]) {
+        const imageUrl = backgroundImageUrlMatch[1]
+        promises.push(updateBackgroundImageWithBase64Image(nodePtr, imageUrl))
+      }
+    }
+    // 如果有 img 和 image 标签，尝试将 src 和 href 替换为 base64 图片
+    if (nodePtr instanceof HTMLImageElement) {
+      promises.push(updateImageSrcOrHrefWithBase64Image(nodePtr, 'src'))
+    } else if (nodePtr instanceof SVGImageElement) {
+      promises.push(updateImageSrcOrHrefWithBase64Image(nodePtr, 'href'))
+    }
+  }
+  await Promise.all(promises)
+}
