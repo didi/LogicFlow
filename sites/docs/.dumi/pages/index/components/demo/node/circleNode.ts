4262b93dde4c05f5a310899103c126ec70856bcb
boyongjiong <boyongjiong@outlook.com>
2024-06-24 16:46:05 +0800

    fix(docs): 解决 docs 启动时「The same observable object cannot appear twice in the same tree」错误
     - 节点定义写法有问题，observer 属性赋值给另一个 observer 属性，导致触发上面错误
     - 更新包版本
     - DEFAULT_GRID_SIZE 将默认的 gridSize 值提成常量值，放在 constant 中，方便修改
     - history 功能需要测试一下 cloneDeep(model) 的部分

--- sites/docs/.dumi/pages/index/components/demo/node/circleNode.ts
+++ sites/docs/.dumi/pages/index/components/demo/node/circleNode.ts
@@ -1,7 +1,8 @@
 import { HtmlNode, HtmlNodeModel, h } from '@logicflow/core';
 
 class CircleNodeView extends HtmlNode {
-  setHtml(rootEl: HTMLElement) {
+  setHtml(rootEl: SVGForeignObjectElement) {
+    const nodeData = this.props.model.getData();
     const { properties } = this.props.model;
     const text: any = properties.text;
     const innerText = text.value;
@@ -11,7 +12,7 @@ class CircleNodeView extends HtmlNode {
     el.className = `step-wrapper circle-wrapper spin ${
       isAnimation ? 'is-animate' : ''
     }`;
-    const html = `<div class='text'>${innerText}</div>`;
+    const html = `<div class='text'>${nodeData.text?.value}</div>`;
     const animationDom = `<div class='border-div border-circle-animate'><div>`;
     // 需要先把之前渲染的子节点清除掉。
     el.innerHTML = isAnimation ? html + animationDom : html;
@@ -25,7 +26,7 @@ class CircleNodeModel extends HtmlNodeModel {
     this.width = 80;
     this.height = 80;
     if (this.text) {
-      this.properties.text = this.text;
+      // this.properties.text = this.text;
       this.text.value = '';
     }
   }
