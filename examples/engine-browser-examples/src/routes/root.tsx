ec350b525a6651ad7f8819b23ee9f5e23b2fe824
boyongjiong <boyongjiong@outlook.com>
2024-07-02 14:30:07 +0800

    feat(examples): 优化 engine-browser-examples 样式

--- examples/engine-browser-examples/src/routes/root.tsx
+++ examples/engine-browser-examples/src/routes/root.tsx
@@ -11,15 +11,12 @@ export default function Root() {
             <input
               id="q"
               aria-label="Search contacts"
-              placeholder="Search"
+              placeholder="Press Enter for Search"
               type="search"
               name="q"
             />
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
-          </form>
-          <form method="post">
-            <button type="submit">New</button>
           </form>
         </div>
         <nav>
