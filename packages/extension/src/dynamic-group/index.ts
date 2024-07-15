1ab9396d6c75335239e4d6fbfc4837ea39133f69
boyongjiong <boyongjiong@outlook.com>
2024-07-15 21:13:48 +0800

    feat: 暂存「开发 动态组节点功能 代码」

--- packages/extension/src/dynamic-group/index.ts
+++ packages/extension/src/dynamic-group/index.ts
@@ -12,12 +12,16 @@ export class DynamicGroup {
   static pluginName = 'dynamicGroup'
 
   private lf: LogicFlow
+  // 激活态的 group 节点
+  activeGroup: any
+  // 存储节点与 group 的映射关系
+  nodeGroupMap: Map<string, string> = new Map()
 
   constructor({ lf }: LogicFlow.IExtensionProps) {
     lf.register(groupNode)
     this.lf = lf
 
-    // 初始化插件，从监听事件开始
+    // 初始化插件，从监听事件开始及设置规则开始
     this.init()
   }
 
@@ -37,13 +41,29 @@ export class DynamicGroup {
       }
       return true
     })
+    graphModel.group = this
 
     this.render()
   }
 
+  /**
+   * 获取分组内的节点
+   * @param groupId
+   */
   getNodesInGroup(groupId: string): string[] {
     console.log('groupId', groupId)
     return []
+  }
+
+  /**
+   * 获取节点所属的分组
+   * @param nodeId
+   */
+  getGroupByNodeId(nodeId: string) {
+    const groupId = this.nodeGroupMap.get(nodeId)
+    if (groupId) {
+      return this.lf.getNodeModelById(groupId)
+    }
   }
 
   render() {}
