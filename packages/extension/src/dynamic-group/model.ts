6b0f64c0915e057f275bf5a7ec264bcc0d0b9883
boyongjiong <boyongjiong@outlook.com>
2024-07-17 20:50:14 +0800

    feat(extension): 调试 dynamicGroup 插件的功能，持续进行代码结构优化

--- packages/extension/src/dynamic-group/model.ts
+++ packages/extension/src/dynamic-group/model.ts
@@ -1,121 +1,15 @@
 import LogicFlow, {
-  h,
-  GraphModel,
-  RectNode,
-  ElementType,
-  RectNodeModel,
   BaseEdgeModel,
+  ElementType,
+  GraphModel,
   IRectNodeProperties,
+  RectNodeModel,
 } from '@logicflow/core'
 import { forEach } from 'lodash-es'
 
 import NodeData = LogicFlow.NodeData
 import NodeConfig = LogicFlow.NodeConfig
 import EdgeConfig = LogicFlow.EdgeConfig
-
-export interface IDynamicGroupNodeProps {
-  model: DynamicGroupNodeModel
-  graphModel: GraphModel
-}
-
-// 分组节点默认展开时的大小
-const DEFAULT_GROUP_EXPAND_WIDTH = 400
-const DEFAULT_GROUP_EXPAND_HEIGHT = 230
-// 分组节点默认收起时的大小
-const DEFAULT_GROUP_COLLAPSE_WIDTH = 80
-const DEFAULT_GROUP_COLLAPSE_HEIGHT = 60
-
-const DEFAULT_BOTTOM_Z_INDEX = -10000
-
-export class DynamicGroupNode<
-  P extends IDynamicGroupNodeProps = IDynamicGroupNodeProps,
-> extends RectNode<P> {
-  getResizeControl(): h.JSX.Element | null {
-    const { resizable, isCollapsed } = this.props.model
-    const showResizeControl = resizable && !isCollapsed
-    return showResizeControl ? super.getResizeControl() : null
-  }
-
-  getAppendAreaShape(): h.JSX.Element | null {
-    // TODO: 此区域用于初始化 group container, 即元素拖拽进入感应区域
-    const { model } = this.props
-    const { width, height, x, y, radius, groupAddable } = model
-    if (!groupAddable) return null
-
-    const { strokeWidth = 0 } = model.getNodeStyle()
-    const style = model.getAddableOutlineStyle()
-
-    const newWidth = width + strokeWidth + 8
-    const newHeight = height + strokeWidth + 8
-    return h('rect', {
-      ...style,
-      width: newWidth,
-      height: newHeight,
-      x: x - newWidth / 2,
-      y: y - newHeight / 2,
-      rx: radius,
-      ry: radius,
-    })
-  }
-
-  getCollapseIcon(sx: number, sy: number): string {
-    return `M ${sx + 3},${sy + 6} ${sx + 11},${sy + 6} M${sx + 7},${sy + 2} ${
-      sx + 7
-    },${sy + 10}`
-  }
-
-  getExpandIcon(sx: number, sy: number): string {
-    return `M ${sx + 3},${sy + 6} ${sx + 11},${sy + 6} `
-  }
-
-  // 获取操作图标（收起或展开）
-  getOperateIcon(): h.JSX.Element | null {
-    const { model } = this.props
-    const { x, y, width, height } = model
-    const sx = x - width / 2 + 5
-    const sy = y - height / 2 + 5
-
-    if (!model.collapsible) return null
-    const iconPath = model?.isCollapsed
-      ? this.getExpandIcon(sx, sy)
-      : this.getCollapseIcon(sx, sy)
-
-    const operateIcon = h('path', {
-      fill: 'none',
-      stroke: '#818281',
-      strokeWidth: 2,
-      'pointer-events': 'none',
-      d: iconPath,
-    })
-
-    return h('g', {}, [
-      h('rect', {
-        height: 12,
-        width: 14,
-        rx: 2,
-        ry: 2,
-        strokeWidth: 1,
-        fill: '#f4f5f6',
-        stroke: '#cecece',
-        cursor: 'pointer',
-        x: sx,
-        y: sy,
-        onClick: () => {
-          model.toggleCollapse(!model.isCollapsed)
-        },
-      }),
-      operateIcon,
-    ])
-  }
-
-  getShape(): h.JSX.Element | null {
-    return h('g', {}, [
-      this.getAppendAreaShape(),
-      super.getShape(),
-      this.getOperateIcon(),
-    ])
-  }
-}
 
 export type IGroupNodeProperties = {
   /**
@@ -171,6 +65,15 @@ export type IGroupNodeProperties = {
   // TODO: 函数类型的 properties 该如何传入
   isAllowAppendIn?: (_nodeData) => boolean
 } & IRectNodeProperties
+
+// 分组节点默认展开时的大小
+const DEFAULT_GROUP_EXPAND_WIDTH = 400
+const DEFAULT_GROUP_EXPAND_HEIGHT = 230
+// 分组节点默认收起时的大小
+const DEFAULT_GROUP_COLLAPSE_WIDTH = 80
+const DEFAULT_GROUP_COLLAPSE_HEIGHT = 60
+
+const DEFAULT_BOTTOM_Z_INDEX = -10000
 
 export class DynamicGroupNodeModel extends RectNodeModel<IGroupNodeProperties> {
   readonly isGroup = true
@@ -589,10 +492,4 @@ export class DynamicGroupNodeModel extends RectNodeModel<IGroupNodeProperties> {
   }
 }
 
-export const dynamicGroup = {
-  type: 'dynamic-group',
-  view: DynamicGroupNode,
-  model: DynamicGroupNodeModel,
-}
-
-export default dynamicGroup
+export default DynamicGroupNodeModel
---
+++ packages/extension/src/dynamic-group/node.ts
@@ -0,0 +1,99 @@
+import { GraphModel, h, RectNode } from '@logicflow/core'
+import { DynamicGroupNodeModel } from './model'
+
+export interface IDynamicGroupNodeProps {
+  model: DynamicGroupNodeModel
+  graphModel: GraphModel
+}
+
+export class DynamicGroupNode<
+  P extends IDynamicGroupNodeProps = IDynamicGroupNodeProps,
+> extends RectNode<P> {
+  getResizeControl(): h.JSX.Element | null {
+    const { resizable, isCollapsed } = this.props.model
+    const showResizeControl = resizable && !isCollapsed
+    return showResizeControl ? super.getResizeControl() : null
+  }
+
+  getAppendAreaShape(): h.JSX.Element | null {
+    // TODO: 此区域用于初始化 group container, 即元素拖拽进入感应区域
+    const { model } = this.props
+    const { width, height, x, y, radius, groupAddable } = model
+    if (!groupAddable) return null
+
+    const { strokeWidth = 0 } = model.getNodeStyle()
+    const style = model.getAddableOutlineStyle()
+
+    const newWidth = width + strokeWidth + 8
+    const newHeight = height + strokeWidth + 8
+    return h('rect', {
+      ...style,
+      width: newWidth,
+      height: newHeight,
+      x: x - newWidth / 2,
+      y: y - newHeight / 2,
+      rx: radius,
+      ry: radius,
+    })
+  }
+
+  getCollapseIcon(sx: number, sy: number): string {
+    return `M ${sx + 3},${sy + 6} ${sx + 11},${sy + 6} M${sx + 7},${sy + 2} ${
+      sx + 7
+    },${sy + 10}`
+  }
+
+  getExpandIcon(sx: number, sy: number): string {
+    return `M ${sx + 3},${sy + 6} ${sx + 11},${sy + 6} `
+  }
+
+  // 获取操作图标（收起或展开）
+  getOperateIcon(): h.JSX.Element | null {
+    const { model } = this.props
+    const { x, y, width, height } = model
+    const sx = x - width / 2 + 5
+    const sy = y - height / 2 + 5
+
+    if (!model.collapsible) return null
+    const iconPath = model?.isCollapsed
+      ? this.getExpandIcon(sx, sy)
+      : this.getCollapseIcon(sx, sy)
+
+    const operateIcon = h('path', {
+      fill: 'none',
+      stroke: '#818281',
+      strokeWidth: 2,
+      'pointer-events': 'none',
+      d: iconPath,
+    })
+
+    return h('g', {}, [
+      h('rect', {
+        height: 12,
+        width: 14,
+        rx: 2,
+        ry: 2,
+        strokeWidth: 1,
+        fill: '#f4f5f6',
+        stroke: '#cecece',
+        cursor: 'pointer',
+        x: sx,
+        y: sy,
+        onClick: () => {
+          model.toggleCollapse(!model.isCollapsed)
+        },
+      }),
+      operateIcon,
+    ])
+  }
+
+  getShape(): h.JSX.Element | null {
+    return h('g', {}, [
+      this.getAppendAreaShape(),
+      super.getShape(),
+      this.getOperateIcon(),
+    ])
+  }
+}
+
+export default DynamicGroupNode
