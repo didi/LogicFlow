ab96e62ef2a3d8ed339dd0634920b5c9a137a982
boyongjiong <boyongjiong@outlook.com>
2024-07-22 11:05:08 +0800

    refactor(core): 重构项目管理
     - 将 snapline 移出 tool 文件夹，因为它本身非 tool，放这里面会有误导
     - 优化 tool 文件夹，将 tool 内容移至 index 中，并更新项目中相关导入

--- packages/core/src/view/behavior/snapline.ts
+++ packages/core/src/view/behavior/snapline.ts
@@ -1,13 +1,12 @@
-import EventEmitter from '../event/eventEmitter'
-import SnaplineModel from '../model/SnaplineModel'
+import EventEmitter from '../../event/eventEmitter'
+import SnaplineModel from '../../model/SnaplineModel'
 
 export function snapline(
   eventCenter: EventEmitter,
   snaplineModel: SnaplineModel,
 ): void {
   // 节点拖动时启动对齐线计算
-  eventCenter.on('node:mousemove', ({ data }: any) => {
-    // TODO: 取消注释，定义 data 类型，解决编译问题
+  eventCenter.on('node:mousemove', ({ data }) => {
     snaplineModel.setNodeSnapLine(data)
   })
   // 节点拖动结束时，对齐线消失
