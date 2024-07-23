206fcb32bf4529ad545fb8724b2018231fb4f238
fanyang <1776336252@qq.com>
2024-07-23 12:56:29 +0800

    fix(site): 恢复图片地址

--- examples/feature-examples/src/pages/extensions/dnd-panel/index.tsx
+++ examples/feature-examples/src/pages/extensions/dnd-panel/index.tsx
@@ -74,7 +74,7 @@ export default function DndPanelExtension() {
           type: 'start',
           text: '开始',
           label: '开始节点',
-          icon: 'https://cdn.jsdelivr.net/gh/Logic-Flow/static@latest/docs/examples/case/bussiness/approve/circle.png',
+          icon: 'https://cdn.jsdelivr.net/gh/Logic-Flow/static@latest/core/start.png',
         },
         {
           type: 'rect',
