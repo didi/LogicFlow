db0896ef2567e3a758be5c729b487176052a99ce
boyongjiong <boyongjiong@outlook.com>
2024-07-30 14:03:06 +0800

    refactor: 移除 examples 中无用的 demo，后续转移到 CodeSandbox 或 github 上维护，用于定位问题

--- examples/vue3-memory-leak/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import FlowView from './components/index.vue'
-</script>
-
-<template>
-  <FlowView />
-</template>
--- examples/vue3-memory-leak/src/components/node-red/readme.md
+++
@@ -1,4 +0,0 @@
-# 说明
-
-参考node-red样式，以logicflow插件的方式实现。
-
