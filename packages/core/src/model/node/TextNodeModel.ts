0c39da4516b8a7f67a8fcbb5e0ef848e30cfacb8
boyongjiong <boyongjiong@outlook.com>
2024-07-31 12:56:44 +0800

    feat: release @logicflow/core & extension 2.0.0-beta.8
     - 解决 BaseEdgeModel properties 类型的问题

--- packages/core/src/model/node/TextNodeModel.ts
+++ packages/core/src/model/node/TextNodeModel.ts
@@ -3,8 +3,18 @@ import { computed } from 'mobx'
 import BaseNodeModel from './BaseNodeModel'
 import { ModelType } from '../../constant'
 import { getSvgTextWidthHeight } from '../../util'
+import LogicFlow from '../../LogicFlow'
 
-export class TextNodeModel extends BaseNodeModel {
+export type ITextNodeProperties = {
+  style?: LogicFlow.CommonTheme
+  textStyle?: LogicFlow.CommonTheme
+
+  [key: string]: unknown
+}
+
+export class TextNodeModel<
+  P extends ITextNodeProperties = ITextNodeProperties,
+> extends BaseNodeModel<P> {
   modelType = ModelType.TEXT_NODE
 
   getTextStyle() {
@@ -14,9 +24,10 @@ export class TextNodeModel extends BaseNodeModel {
     return {
       ...style,
       ...cloneDeep(text),
-      ...cloneDeep(textStyle as object),
+      ...cloneDeep(textStyle),
     }
   }
+
   @computed get width(): number {
     const rows = String(this.text.value).split(/[\r\n]/g)
     const { fontSize } = this.getTextStyle()
