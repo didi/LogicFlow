67dc7b9e9358c6ecdf126cfb0c641618aa97310d
didi <didi@MacBook-Pro-2.local>
2024-08-13 11:48:18 +0800

    fix(extension): 修改样式打包配置,用于修复引入插件包后起服务报错问题(issue: #1760)

--- packages/extension/rollup.config.js
+++ packages/extension/rollup.config.js
@@ -1,3 +1,19 @@
+import postcss from 'rollup-plugin-postcss'
+import postcssImport from 'postcss-import'
+import postcssUrl from 'postcss-url'
 import { rollupConfig } from '../../rollup.config'
 
-export default rollupConfig({})
+export default rollupConfig({
+  plugins: [
+    postcss({
+      plugins: [
+        postcssImport(), // 处理 @import 语法
+        postcssUrl({
+          url: 'inline', // 选择 'inline' 选项将外部资源内联到最终的 CSS 文件中
+          maxSize: 10, // 以KB为单位的最大文件大小，超过此大小的文件将不会被内联
+        }),
+      ],
+      extract: true, // 提取到一个单独的 CSS 文件
+    }),
+  ],
+})
