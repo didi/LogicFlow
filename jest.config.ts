522f71e108efb1107d21439e2bb792385674a8c0
seanzhu <seanzhu@didiglobal.com>
2026-04-02 20:34:58 +0800

    fix: 修复测试用例

---
+++ jest.config.ts
@@ -0,0 +1,46 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+
+  // A set of global variables that need to be available in all test environments
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
+
+  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
+  moduleNameMapper: {
+    '^lodash-es$': 'lodash',
+  },
+
+  // A list of paths to modules that run some code to configure or set up the testing framework before each test
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+
+  // The test environment that will be used for testing
+  testEnvironment: 'jest-environment-jsdom',
+
+  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/es/',
+    '/lib/',
+    '/examples/vue3-app/',
+  ],
+
+  // A map from regular expressions to paths to transformers
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+
+  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+  transformIgnorePatterns: ['node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)'],
+}
+
+export default config
--- packages/extension/__test__/bpmn-adapter/bpmn-adapter.test.ts
+++ packages/extension/__test__/bpmn-adapter/bpmn-adapter.test.ts
@@ -1,10 +1,11 @@
-import { toXmlJson, toNormalJson } from '../src/bpmn-adapter/index'
-import { lfJson2Xml } from '../src/bpmn-adapter/json2xml'
-import { lfXml2Json } from '../src/bpmn-adapter/xml2json'
-
 /**
  * @jest-environment jsdom
  */
+
+import { toXmlJson, toNormalJson } from '../../src/bpmn-adapter/index'
+import { lfJson2Xml } from '../../src/bpmn-adapter/json2xml'
+import { lfXml2Json } from '../../src/bpmn-adapter/xml2json'
+
 test('transform data from json to xml', () => {
   const testObj = {
     nodes: [
@@ -169,59 +170,61 @@ test('transform data from json to xml', () => {
   })
 
   expect(lfJson2Xml(xmlJson)).toStrictEqual(
-    '\t\n  <nodes id="node_1" type="rect" x="100" y="200" properties="{}" />\t\n  <nodes id="node_2" type="circle" x="300" y="160" properties="{}" />\t\n  <edges id="ca1dea84-c5e8-4344-b888-8bb09666ac42" type="polyline" sourceNodeId="node_1" targetNodeId="node_2" startPoint="{\'x\':150,\'y\':200}" endPoint="{\'x\':250,\'y\':160}" properties="{}" pointsList="[{\'x\':150,\'y\':200},{\'x\':200,\'y\':200},{\'x\':200,\'y\':160},{\'x\':250,\'y\':160}]" />',
+    '\t\n  <nodes id="node_1" type="rect" x="100" y="200" properties="{}" />\t\n  <nodes id="node_2" type="circle" x="300" y="160" properties="{}" />\t\n  <edges id="ca1dea84-c5e8-4344-b888-8bb09666ac42" type="polyline" sourceNodeId="node_1" targetNodeId="node_2" startPoint="{&apos;x&apos;:150,&apos;y&apos;:200}" endPoint="{&apos;x&apos;:250,&apos;y&apos;:160}" properties="{}" pointsList="[{&apos;x&apos;:150,&apos;y&apos;:200},{&apos;x&apos;:200,&apos;y&apos;:200},{&apos;x&apos;:200,&apos;y&apos;:160},{&apos;x&apos;:250,&apos;y&apos;:160}]" />',
   )
 
   const xml = lfJson2Xml(xmlJson)
-  expect(lfXml2Json(xml)).toStrictEqual({
-    nodes: [
-      {
-        '-id': 'node_1',
-        '-type': 'rect',
-        '-x': 100,
-        '-y': 200,
-        '-properties': {},
-      },
-      {
-        '-id': 'node_2',
-        '-type': 'circle',
-        '-x': 300,
-        '-y': 160,
-        '-properties': {},
-      },
-    ],
-    edges: {
-      '-id': 'ca1dea84-c5e8-4344-b888-8bb09666ac42',
-      '-type': 'polyline',
-      '-sourceNodeId': 'node_1',
-      '-targetNodeId': 'node_2',
-      '-startPoint': {
-        x: 150,
-        y: 200,
-      },
-      '-endPoint': {
-        x: 250,
-        y: 160,
-      },
-      '-properties': {},
-      '-pointsList': [
+  expect(lfXml2Json(`<root>${xml}</root>`)).toStrictEqual({
+    root: {
+      nodes: [
         {
+          '-id': 'node_1',
+          '-type': 'rect',
+          '-x': 100,
+          '-y': 200,
+          '-properties': {},
+        },
+        {
+          '-id': 'node_2',
+          '-type': 'circle',
+          '-x': 300,
+          '-y': 160,
+          '-properties': {},
+        },
+      ],
+      edges: {
+        '-id': 'ca1dea84-c5e8-4344-b888-8bb09666ac42',
+        '-type': 'polyline',
+        '-sourceNodeId': 'node_1',
+        '-targetNodeId': 'node_2',
+        '-startPoint': {
           x: 150,
           y: 200,
         },
-        {
-          x: 200,
-          y: 200,
-        },
-        {
-          x: 200,
-          y: 160,
-        },
-        {
+        '-endPoint': {
           x: 250,
           y: 160,
         },
-      ],
+        '-properties': {},
+        '-pointsList': [
+          {
+            x: 150,
+            y: 200,
+          },
+          {
+            x: 200,
+            y: 200,
+          },
+          {
+            x: 200,
+            y: 160,
+          },
+          {
+            x: 250,
+            y: 160,
+          },
+        ],
+      },
     },
   })
 })
